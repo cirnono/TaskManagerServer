2459d4d92072bd87279962d2abe0439391627ce2
cirnono <gugee.chen@gmail.com>
2025-07-31 00:27:30 +1000

    fix typo

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -5,39 +5,39 @@ import { asyncHandler } from "./asyncHandler";
 
 // Check if user authenticated
 export const authticate = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies.jwt;
+    async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.cookies.jwt;
 
-    if (!token) {
-      res.status(401);
-      throw new Error("Not authorised, token failed");
+        if (!token) {
+            res.status(401);
+            throw new Error("Not authorised, token failed");
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+            userId: string;
+        };
+        const user = (await User.findById(decoded.userId).select(
+            "-passwordHash"
+        )) as IUser | null;
+
+        if (!user) {
+            res.status(401);
+            throw new Error("Not authorised, no token");
+        }
+
+        req.user = user;
+        next();
     }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
-      userId: string;
-    };
-    const user = (await User.findById(decoded.userId).select(
-      "-password"
-    )) as IUser | null;
-
-    if (!user) {
-      res.status(401);
-      throw new Error("Not authorised, no token");
-    }
-
-    req.user = user;
-    next();
-  }
 );
 
 export const authorisedAdmin = (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  if (req.user?.isAdmin) {
-    next();
-  } else {
-    res.status(401).send("Not authorised as an admin");
-  }
+    if (req.user?.isAdmin) {
+        next();
+    } else {
+        res.status(401).send("Not authorised as an admin");
+    }
 };
